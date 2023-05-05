7eb7cbcba910aeac19086fe768a61964c8073a43
Vasilis Kefallinos <vkefallinos@gmail.com>
2023-05-05 22:55:00 +0300

    fixes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from 'lib/db-client/Store'
+import { supabase } from 'lib/api/Store'
 
 const Home = () => {
   const [username, setUsername] = useState('')
